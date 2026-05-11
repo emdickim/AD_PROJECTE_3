62a28b89557997862fdebc512773b2365ddfdb7c
Kim Làzaro Pardo <kimlazaropardo@gmail.com>
2026-05-11 16:05:40 +0200

    Add files via upload

---
+++ projecte3/src/main/java/com/ra5/projecte3/Projecte3Application.java
@@ -0,0 +1,13 @@
+package com.ra5.projecte3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projecte3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projecte3Application.class, args);
+	}
+
+}
